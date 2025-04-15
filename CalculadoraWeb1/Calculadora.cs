ff9d496abdd4842903acce581876000102599a41
Anthony <anthonysouza75@hotmail.com.br>
2025-04-15 14:57:52 -0400

    novos arquivos

---
+++ CalculadoraWeb1/Calculadora.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CalculadoraWeb1
+{
+	public class Calculadora
+	{
+		public List<int> GerarNumerosFibonacci(int numero)
+		{
+			var listaFibonacci = new List<int>();
+			listaFibonacci.Add(1);
+			listaFibonacci.Add(1);
+
+			for (int i = 2; i < numero; i++) 
+			{
+
+				var novoValor = listaFibonacci[i - 1] + listaFibonacci[i - 2];
+				listaFibonacci.Add(novoValor);
+				
+			}
+			return listaFibonacci;
+		}
+		public void TestarTeoriaDeInstanciacao(int numero, string nome, bool eh) 
+		{
+			
+		}
+	}
+}
