b9892f7e47e1111787a85c159f2ab69aa043766e
Anthony <anthonysouza75@hotmail.com.br>
2025-04-22 14:53:27 -0400

    Substitui o zero na calculadora

--- CalculadoraWeb1/CalculadoraCompleta.aspx.cs
+++ CalculadoraWeb1/CalculadoraCompleta.aspx.cs
@@ -35,47 +35,74 @@ namespace CalculadoraWeb1
 
 		protected void Numero7Button_Click(object sender, EventArgs e)
 		{
-			NumeroAtual = NumeroAtual + "7";
+			if (NumeroAtual == "0")
+				NumeroAtual = "7";
+			else
+				NumeroAtual = NumeroAtual + "7";
 		}
 
 		protected void Numero8Button_Click(object sender, EventArgs e)
 		{
-			NumeroAtual = NumeroAtual + "8";
+			if (NumeroAtual == "0")
+				NumeroAtual = "8";
+			else
+				NumeroAtual = NumeroAtual + "8";
 		}
 
 		protected void Numero9Button_Click(object sender, EventArgs e)
 		{
-			NumeroAtual = NumeroAtual + "9";
+			if (NumeroAtual == "0")
+				NumeroAtual = "9";
+			else
+				NumeroAtual = NumeroAtual + "9";
 		}
 
 		protected void Numero4Button_Click(object sender, EventArgs e)
 		{
-			NumeroAtual = NumeroAtual + "4";
+			if (NumeroAtual == "0")
+				NumeroAtual = "4";
+			else
+				NumeroAtual = NumeroAtual + "4";
 		}
 
 		protected void Numero5Button_Click(object sender, EventArgs e)
 		{
-			NumeroAtual = NumeroAtual + "5";
+			if (NumeroAtual == "0")
+				NumeroAtual = "5";
+			else
+				NumeroAtual = NumeroAtual + "5";
 		}
 
 		protected void Numero6Button_Click(object sender, EventArgs e)
 		{
-			NumeroAtual = NumeroAtual + "6";
+			if (NumeroAtual == "0")
+				NumeroAtual = "6";
+			else
+				NumeroAtual = NumeroAtual + "6";
 		}
 
 		protected void Numero1Button_Click(object sender, EventArgs e)
 		{
-			NumeroAtual = NumeroAtual + "1";
+			if (NumeroAtual == "0")
+				NumeroAtual = "1";
+			else
+				NumeroAtual = NumeroAtual + "1";
 		}
 
 		protected void Numero2Button_Click(object sender, EventArgs e)
 		{
-			NumeroAtual = NumeroAtual + "2";
+			if (NumeroAtual == "0")
+				NumeroAtual = "2";
+			else
+				NumeroAtual = NumeroAtual + "2";
 		}
 
 		protected void Numero3Button_Click(object sender, EventArgs e)
 		{
-			NumeroAtual = NumeroAtual + "3";
+			if (NumeroAtual == "0")
+				NumeroAtual = "3";
+			else
+				NumeroAtual = NumeroAtual + "3";
 		}
 
 		protected void Numero0Button_Click(object sender, EventArgs e)
