06c0b522e7712075ff66a23434470d67c418ffcb
Anthony <anthonysouza75@hotmail.com.br>
2025-05-06 13:27:07 -0400

    att

---
+++ ContaBancaria/DAOs/CadastroUsuarioDao.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Sql;
+using System.Configuration;
+namespace ContaBancaria.DAOs
+{
+	public class CadastroUsuarioDao //Classe que conversa com o banco de dados
+	{
+		private readonly string _conexao;
+
+		public CadastroUsuarioDao(string conexao)
+		{
+			_conexao = conexao;
+		}
+
+		public void CadastrarUsuario()
+		{
+			
+		}
+	}
+}
